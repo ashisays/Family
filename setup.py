2062f14e229094de7131acc379eddfeb53bbcec6
ASHISH PUNDIR <pundir.ashish@live.com>
2020-06-28 14:47:32 +0530

    testcase for person file.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='family',
+    version='0.1.0',
+    description='geektrusk family problem solutions',
+    author='Ashish Pundir',
+    author_email='pundir.pd@gmail.com',
+    license='MIT License',
+    packages=['src/family'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Review/Coding',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.x',
+    ],
+)
--- src/tests/test_person.py
+++ src/tests/test_person.py
@@ -1,9 +1,15 @@
 from unittest import TestCase
-
+from src.family import person
 
 class TestPerson(TestCase):
+    def setUp(self):
+        self.boy = person.Male("a", None)
+        self.girl = person.Female("a", None)
+
     def test_is_boy(self):
-        self.fail()
+        if not self.boy.isBoy():
+            self.fail("Testcase failed , exected True found %s" %self.boy.isBoy())
 
     def test_is_girl(self):
-        self.fail()
+        if not self.girl.isGirl():
+            self.fail("Testcase failed , exected True found %s" % self.girl.isGirl())
